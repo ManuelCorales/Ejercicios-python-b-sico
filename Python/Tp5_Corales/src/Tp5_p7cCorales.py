411680320df9d31dbcfd4e642241505a78a13018
Manuel <manuelcorales0712@gmail.com>
2019-06-06 18:34:26 -0300

    Ejercicio p7 tp5 terminado por completo

---
+++ Python/Tp5_Corales/src/Tp5_p7cCorales.py
@@ -0,0 +1,154 @@
+# Los números tienen que ser de 10 cifras
+import sys
+
+
+def loop_preguntar_nombre():
+    """Esta función es la encargada de ejecutar todas las demás
+    No recibe ni devuelve ningun parametro, por lo tanto no es
+    considerada testeable"""
+    dic_nombre = {"carlos": "1165329856",
+                  "ruben": "1123654875",
+                  "jorge": "1156236235"}
+    while True:
+        nombre = preguntar_nombre("")
+        if nombre == "Error":
+            continue
+        elif nombre == "Terminar programa":
+            sys.exit(0)
+        if proceso_nombre(nombre, dic_nombre) == "No se encontró el nombre":
+            rta_crear_o_no_telefono = crear_o_no_telefono("")
+            if rta_crear_o_no_telefono == "Crear teléfono":
+                nuevo_telefono = crear_telefono(nombre, "")
+                if nuevo_telefono == "Error":
+                    print("Ingresó cualquier cosa, por lo tanto no se va",
+                          "a crear el telefono de " + nombre)
+                    continue
+                elif nuevo_telefono == "Volver a preguntar nombre":
+                    continue
+                else:
+                    dic_nombre[nombre] = nuevo_telefono
+                    print("Teléfono creado correctamente")
+        else:
+            rta_si_o_no_editar_telefono = si_o_no_editar_telefono("")
+            if rta_si_o_no_editar_telefono == "Editar teléfono":
+                telefono_editado = editar_telefono(nombre, "")
+                if telefono_editado == "Error":
+                    print("Ingresó cualquier cosa, por lo tanto no se va",
+                          "a editar el telefono de " + nombre)
+                    continue
+                elif telefono_editado == "Volver a preguntar nombre":
+                    continue
+                else:
+                    dic_nombre[nombre] = telefono_editado
+                    print("Teléfono editado correctamente")
+
+
+def crear_telefono(nombre, nuevo_telefono):
+    """Recibe el parametro nombre y junto con 'nuevo_telefono'
+    """
+    print("Introduzca el nuevo telefono que quiere asignarle a " + nombre)
+    nuevo_telefono = inputs(nuevo_telefono)
+    try:
+        nuevo_telefono = int(nuevo_telefono)
+        nuevo_telefono = str(nuevo_telefono)
+        if len(nuevo_telefono) == 10:
+            return nuevo_telefono
+        else:
+            return "Error"
+    except Exception:
+        print("Solo se pueden ingresar números")
+        return "Error"
+
+
+def editar_telefono(nombre, telefono_editado):
+    """Recibe los dos parametros y con ellos hace un print.
+    Además, devuelve el número de teléfono editado"""
+    print("Introduzca el nuevo telefono que quiere asignarle a " + nombre)
+    telefono_editado = inputs(telefono_editado)
+    try:
+        telefono_editado = int(telefono_editado)
+        telefono_editado = str(telefono_editado)
+        if len(telefono_editado) == 10:
+            return telefono_editado
+        else:
+            return "Error"
+    except Exception:
+        print("Solo se pueden ingresar números")
+        return "Error"
+
+
+def si_o_no_editar_telefono(si_o_no):
+    """Recibe 'si_o_no' unicamente para ser testeada
+    y devuelve 'Volver a preguntar nombre' en caso de
+    que no se quiera editar el número. Y en caso contrario
+    devuelve 'Editar teléfono'"""
+    print("Desea editar el teléfono? Escriba 'Si', o 'No'")
+    si_o_no = ((inputs(si_o_no)).lower()).strip()
+    if si_o_no == "no":
+        return "Volver a preguntar nombre"
+    elif si_o_no == "si":
+        return "Editar teléfono"
+
+
+def crear_o_no_telefono(si_o_no):
+    print("Desea crear un teléfono para ese contacto? Ingrese 'Si' o 'No'")
+    si_o_no = ((inputs(si_o_no)).lower()).strip()
+    if si_o_no == "no":
+        return "No crear"
+    elif si_o_no == "si":
+        return "Crear teléfono"
+
+
+def inputs(var):
+    """Se encarga de ejecutar un input cada vez que se la llama
+    siendo ejecutada desde el mismo archivo. En caso de que el
+    programa sea ejecutado por otro archivo, devuelve la misma
+    variable que se le ingresa"""
+    if __name__ == "__main__":
+        var = input()
+        return var
+    else:
+        return var
+
+
+def preguntar_nombre(nombre):
+    """Evalúa si la nombre ingresada no contiene números
+    ni esta vacía. En cualquiera de los dos casos devuelve
+    'Error'"""
+    print("Teléfono de quién desea saber?")
+    nombre = inputs(nombre)
+    try:
+        if nombre.strip() == "":
+            print("No se puede introducir solo espacios o nada")
+            return "Error"
+        nombre = int(nombre)
+        print("No se pueden introducir solo números")
+        return "Error"
+    except Exception:
+        for i in range(10):
+            i = str(i)
+            letra = nombre.find(i)
+            try:
+                letra = int(letra)
+                if letra != -1:
+                    print("No se puede introducir números a la vez que letras")
+                    return "Error"
+            except Exception:
+                pass
+    if nombre.strip() == "*":
+        return "Terminar programa"
+    return (nombre.strip()).lower()
+
+
+def proceso_nombre(nombre, dic_nombre):
+    """Recibe el nombre ingresado junto con el diccionario de nombres.
+    Lo busca, y si lo encuentra devuelve: 'No se encontró el nombre'
+    y devuelve el teléfono en caso contrario"""
+    if nombre in dic_nombre:
+        print("El telefono de " + nombre + " es: " + dic_nombre[nombre])
+        return dic_nombre[nombre]
+    else:
+        print("No se encontró el nombre")
+        return "No se encontró el nombre"
+if __name__ == "__main__":
+    loop_preguntar_nombre()
---
+++ Python/Tp5_Corales/tests/Tp5_testTp5_p7cCorales.py
@@ -0,0 +1,122 @@
+# No es necesario testear la función loop_preguntar_nombre debido a que
+# no devuleve nada y lo unico que hace es iterar otra función hasta que se dé
+# una condición
+
+# Tampoco es necesario testear la función inputs debido a que lo unico de
+# lo que se encarga es realizar inputs cada vez que se la llama desde el
+# mismo archivo y devuelve ese valor de input. En caso de ser ejecutada
+# desde algun archivo externo, devuelve absolutamente lo mismo que se le
+# ingresa
+
+# Diccionario: nombres_telefonos = {"carlos": "1165329856",
+#                                   "ruben": "1123654875"
+#                                   "jorge": "1156236235"}
+
+# Los teléfonos tienen que ser de 10 números
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import src.Tp5_p7cCorales  # noqa
+
+
+class TestTp5_p7cCorales(unittest.TestCase):
+    def test_si_o_no_editar_telefono(self):
+        """Testea si segun ingresa 'si' o 'no' si decide o no editar
+        teléfono"""
+        self.assertEqual(src.Tp5_p7cCorales.si_o_no_editar_telefono("Si"),
+                         ("Editar teléfono"))
+        self.assertEqual(src.Tp5_p7cCorales.si_o_no_editar_telefono("si"),
+                         ("Editar teléfono"))
+        self.assertEqual(src.Tp5_p7cCorales.si_o_no_editar_telefono("No"),
+                         ("Volver a preguntar nombre"))
+        self.assertEqual(src.Tp5_p7cCorales.si_o_no_editar_telefono("no"),
+                         ("Volver a preguntar nombre"))
+
+    def test_crear_o_no_telefono(self):
+        """Testea si segun ingresa 'si' o 'no' si decide o no crear
+        teléfono"""
+        self.assertEqual(src.Tp5_p7cCorales.crear_o_no_telefono("Si"),
+                         ("Crear teléfono"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_o_no_telefono("si"),
+                         ("Crear teléfono"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_o_no_telefono("No"),
+                         ("No crear"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_o_no_telefono("no"),
+                         ("No crear"))
+
+    def test_editar_telefono(self):
+        """Testea en qué casos devuelve error si se mete cualquier
+        otra cosa que no sea un número y tenga 10 cifras"""
+        self.assertEqual(src.Tp5_p7cCorales.editar_telefono
+                         ("Carlos", "1156982059"),
+                         ("1156982059"))
+        self.assertEqual(src.Tp5_p7cCorales.editar_telefono
+                         ("Carlos", "asdasd"),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.editar_telefono
+                         ("Carlos", "asd1156982059"),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.editar_telefono
+                         ("Carlos", ""),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.editar_telefono
+                         ("Carlos", "115698205965465135"),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.editar_telefono
+                         ("Maxi", "11059"),
+                         ("Error"))
+
+    def test_crear_telefono(self):
+        """Testea en qué casos devuelve error si se mete cualquier
+        otra cosa que no sea un número y tenga 10 cifras"""
+        self.assertEqual(src.Tp5_p7cCorales.crear_telefono
+                         ("Juan Jesús de la Rosa", "1156652059"),
+                         ("1156652059"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_telefono
+                         ("Elba nana", "asdasd"),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_telefono
+                         ("", "asd1156982059"),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_telefono
+                         ("Estebean Rogelio", ""),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_telefono
+                         ("Carlos", "115698205965465135"),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.crear_telefono
+                         ("Maxi", "11059"),
+                         ("Error"))
+
+    def test_proceso_nombre(self):
+        """Testea cada una de los nombres ingresados comprobando que devuelva
+        sus respectivos números de teléfono"""
+        self.assertEqual(src.Tp5_p7cCorales.proceso_nombre
+                         ("carlos", {"carlos": "1165329856"}),
+                         ("1165329856"))
+        self.assertEqual(src.Tp5_p7cCorales.proceso_nombre
+                         ("jorge", {"jorge": "1156236235"}),
+                         ("1156236235"))
+        self.assertEqual(src.Tp5_p7cCorales.proceso_nombre
+                         ("ruben", {"ruben": "1123654875"}),
+                         ("1123654875"))
+
+    def test_preguntar_nombre(self):
+        """Llama a la funcion preguntar_nombre con cada palabra y devuelve
+        si pasa los filtros de la cadena ingresada"""
+        self.assertEqual(src.Tp5_p7cCorales.preguntar_nombre("Carlos"),
+                         ("carlos"))
+        self.assertEqual(src.Tp5_p7cCorales.preguntar_nombre("Elba Nana"),
+                         ("elba nana"))
+        self.assertEqual(src.Tp5_p7cCorales.preguntar_nombre("RUBEN "),
+                         ("ruben"))
+        self.assertEqual(src.Tp5_p7cCorales.preguntar_nombre
+                         ("Aaa 123"), ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.preguntar_nombre("   "),
+                         ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.preguntar_nombre
+                         ("123"), ("Error"))
+        self.assertEqual(src.Tp5_p7cCorales.preguntar_nombre("*"),
+                         ("Terminar programa"))
+unittest.main()
